1c64ccf53ec81d96e62725617dd0eb2b2ebbacbb
Rudra swain <rudrasankar96@gmail.com>
2025-07-11 13:21:36 +0530

    Add getMark function to evaluate student grades with early return pattern

--- JS_by_Sheryians Coding School/Practice.js
+++ JS_by_Sheryians Coding School/Practice.js
@@ -105,3 +105,16 @@ function getGrade(score){
     }
 }
 console.log(getGrade(26));
+
+// Early retuen pattern example.
+
+function getMark(score){
+    if(score >=90 && score <=100) return "A+";
+    if(score >=80 && score <=89) return "A";
+    if(score >=70 && score <=79) return "B";
+    if(score >=60 && score <=69) return "C";
+    if(score >=33 && score <=59) return "D";
+    if(score >=0 && score <=32) return "Fail";
+    return "Invalid marks";    
+}
+console.log(getMark(-2)); // Output: Fail
