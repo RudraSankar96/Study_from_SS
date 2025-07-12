5bb64c9df205fc612b7c7ef9763a1ab39cbbbc48
Rudra swain <rudrasankar96@gmail.com>
2025-07-12 12:57:13 +0530

    Refactor loop examples in Loops.js for improved clarity and organization

--- JS_by_Sheryians Coding School/Loops.js
+++ JS_by_Sheryians Coding School/Loops.js
@@ -3,24 +3,24 @@
 
 // 1. Print numbers frim 1 to 10 using for loop.
 
-// for (let i=1; i<=10; i++){
-//     console.log(i);
-// }
+for (let i=1; i<=10; i++){
+    console.log(i);
+}
 
-// // 2. Print numbers from 10 to 1 using while loop.
+// 2. Print numbers from 10 to 1 using while loop.
 
-// let j=10;
-// while (j >= 1){
-//     console.log(j);
-//     j--;
-// }
+let j=10;
+while (j >= 1){
+    console.log(j);
+    j--;
+}
 
-// // 3. Print even numbers from 1 to 20 using for loop.
-// for ( let n=1; n<=20; n++){
-//     if (n % 2 === 0){
-//         console.log(n);
-//     }
-// }
+// 3. Print even numbers from 1 to 20 using for loop.
+for ( let n=1; n<=20; n++){
+    if (n % 2 === 0){
+        console.log(n);
+    }
+}
 
 // 4. Print odd numbers from 1 to 15 using while loop.
 let m=1;
@@ -28,4 +28,9 @@ while( m<= 15){
      if (m % 2 === 1){
     console.log(m);}
     m++;
+}
+
+// 5. Print the multiplication table of 5 using for loop.
+for (let k =1; k<=10; k++){
+    console.log(`5 * ${k} = ${ 5 * k}`);
 }
