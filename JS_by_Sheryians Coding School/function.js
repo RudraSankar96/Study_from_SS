79454add83ee5019780d4eea199ea3d13a42dd55
Rudra swain <rudrasankar96@gmail.com>
2025-07-18 23:34:30 +0530

    Giving a example of closure

--- JS_by_Sheryians Coding School/function.js
+++ JS_by_Sheryians Coding School/function.js
@@ -185,3 +185,24 @@ function findMax(a, b, c) {
   }
 }
 console.log(findMax(4, 9, 2)); // Output: 9
+console.log("....................");
+
+//  Create a closure that adds a given number to any number you pass later.
+function makeAdder(x) {
+  return function(y) {
+    return x + y;
+  };
+}
+
+// Creating a closure
+const add5 = makeAdder(5);  // x = 5 is remembered
+
+console.log(add5(10)); // Output: 15 → (5 + 10)
+console.log(add5(20)); // Output: 25 → (5 + 20)
+
+// You can create other versions too
+const add10 = makeAdder(10);
+console.log(add10(7)); // Output: 17 → (10 + 7)
+console.log("..................");
+
+
--- JS_by_Sheryians Coding School/tempCodeRunnerFile.js
+++ JS_by_Sheryians Coding School/tempCodeRunnerFile.js
@@ -1,8 +1,15 @@
-function findMax(a, b, c) {
-  if(a>b && a>c) return "a is greater";
-  if(b>a && b>c) return b;
-  else{
-    return "c is greater"
-  }
+function makeAdder(x) {
+  return function(y) {
+    return x + y;
+  };
 }
-console.log(findMax(4, 9, 2)); // Output: 9
+
+// Creating a closure
+const add5 = makeAdder(5);  // x = 5 is remembered
+
+console.log(add5(10)); // Output: 15 → (5 + 10)
+console.log(add5(20)); // Output: 25 → (5 + 20)
+
+// You can create other versions too
+const add10 = makeAdder(10);
+console.log(add10(7)); // Output: 17 → (10 + 7)
