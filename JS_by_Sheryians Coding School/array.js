bff81c46225291e0ba72c9e6fb0eca1dc6693631
Rudra swain <rudrasankar96@gmail.com>
2025-07-22 21:33:48 +0530

    Example of reverse method

--- JS_by_Sheryians Coding School/array.js
+++ JS_by_Sheryians Coding School/array.js
@@ -1,40 +1,40 @@
 // Hoe to right array in js 
 // its reprsent between two [], and arrays have index value .
-let marks = [45,55,23,65,89,99,100]
+let marks = [45,55,23,65,89,99,100];
 console.log(marks[4]);
 console.log("..................");
 
 // modify array:
-let arr = [10,20,30,40,50,60,]
+let arr = [10,20,30,40,50,60,];
 arr[3] = 80;
 console.log(arr);
 console.log("..................");
 
 // Example of push method.
 // add a number in the last index of array.
-let arr2 = [11,22,33,44,55,66]
+let arr2 = [11,22,33,44,55,66];
 arr2.push(88);
 console.log(arr2);
 console.log("....................");
 
 // Example of pop method.
 // delete the last index value.
-let num = [ 74,85,96,41,52,63]
-num.pop()
+let num = [ 74,85,96,41,52,63];
+num.pop();
 console.log(num);
 console.log("....................");
 
 // Example of shift method.
 // its remove first  item from  array.
-let str = [45,55,68,21]
+let str = [45,55,68,21];
 str.shift();
 console.log(str);
 console.log("....................");
 
 // Example of unshift method.
 // It add item at the begining of array.
-let name = ["Happy","Rudra", "Sankar"]
-name.unshift("Bikun")
+let name = ["Happy","Rudra", "Sankar"];
+name.unshift("Bikun");
 console.log(name);
 console.log(".....................");
 
@@ -44,3 +44,9 @@ let name2 = ["Happy", "Rudra", "Sankar"];
 name2.splice(1, 0, "Bikun"); // Insert at index 1
 console.log(name2);
 console.log(".....................");
+
+// Example of reverse method.
+// It reverse the order no of element.
+let a = [4,5,6,7,8,9];
+a.reverse();
+console.log(a);
