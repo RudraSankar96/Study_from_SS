5c475d2aa7231b03b9c555a9dcf8e70bde6a568e
Rudra swain <rudrasankar96@gmail.com>
2025-07-22 21:07:21 +0530

    Example of push method

--- JS_by_Sheryians Coding School/array.js
+++ JS_by_Sheryians Coding School/array.js
@@ -10,3 +10,8 @@ arr[3] = 80;
 console.log(arr);
 console.log("..................");
 
+// Example of push method.
+let arr2 = [11,22,33,44,55,66]
+arr2.push(88);
+console.log(arr2);
+console.log("....................");
---
+++ JS_by_Sheryians Coding School/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+// Example of push method.
+let arr2 = [11,22,33,44,55,66]
+arr2.push(88);
+console.log(arr2);
+console.log("....................");
