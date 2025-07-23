13f1d427c518cfc825a91a27eb90319d9c0dd3f8
Rudra swain <rudrasankar96@gmail.com>
2025-07-23 21:56:41 +0530

    Print the even number using fillter method

--- JS_by_Sheryians Coding School/array.js
+++ JS_by_Sheryians Coding School/array.js
@@ -96,3 +96,14 @@ newnum.sort(function (a, b) {
 });
 console.log(newnum);
 console.log("..........................");
+
+// Print the even number using fillter() method. And sort them in asecnding order.
+let num2 = [45,36,89,99,90,98,2,12];
+let newarr= num2.filter(function(val){
+    if(val % 2 === 0) return true ;
+});
+newarr.sort(function(a,b){
+    return a -b ;
+});
+console.log(newarr);
+console.log(".........................");
--- JS_by_Sheryians Coding School/tempCodeRunnerFile.js
+++ JS_by_Sheryians Coding School/tempCodeRunnerFile.js
@@ -1,12 +1,10 @@
-// Example of fillter() method.
-// Print the number which is greater than 12.
-let num1 =[1,2,3,4,5,6,7,8,9,4,5,6,12,3,55,66,,44,78];
-let newnum=num1.filter(function(val){
-    if(val > 12 ) return true;
-    
+// Print the even number using fillter() method. And sort them in asecnding order.
+let num2 = [45,36,89,99,90,98,2,12];
+let newarr= num2.filter(function(val){
+    if(val % 2 === 0) return true ;
 });
-newnum.sort(function(a,b){
-        return a - b;
-    });
-console.log(newnum);
-console.log("..........................");
+newarr.sort(function(a,b){
+    return a -b ;
+});
+console.log(newarr);
+console.log(".........................");
