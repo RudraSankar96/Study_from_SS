18658fae1b899c00e59544e342572428f068825c
Rudra swain <rudrasankar96@gmail.com>
2025-07-23 21:51:45 +0530

    Example of fillter method

--- JS_by_Sheryians Coding School/array.js
+++ JS_by_Sheryians Coding School/array.js
@@ -1,39 +1,39 @@
-// Hoe to right array in js 
+// Hoe to right array in js
 // its reprsent between two [], and arrays have index value .
-let marks = [45,55,23,65,89,99,100];
+let marks = [45, 55, 23, 65, 89, 99, 100];
 console.log(marks[4]);
 console.log("..................");
 
 // modify array:
-let arr = [10,20,30,40,50,60,];
+let arr = [10, 20, 30, 40, 50, 60];
 arr[3] = 80;
 console.log(arr);
 console.log("..................");
 
 // Example of push method.
 // add a number in the last index of array.
-let arr2 = [11,22,33,44,55,66];
+let arr2 = [11, 22, 33, 44, 55, 66];
 arr2.push(88);
 console.log(arr2);
 console.log("....................");
 
 // Example of pop method.
 // delete the last index value.
-let num = [ 74,85,96,41,52,63];
+let num = [74, 85, 96, 41, 52, 63];
 num.pop();
 console.log(num);
 console.log("....................");
 
 // Example of shift method.
 // its remove first  item from  array.
-let str = [45,55,68,21];
+let str = [45, 55, 68, 21];
 str.shift();
 console.log(str);
 console.log("....................");
 
 // Example of unshift method.
 // It add item at the begining of array.
-let name = ["Happy","Rudra", "Sankar"];
+let name = ["Happy", "Rudra", "Sankar"];
 name.unshift("Bikun");
 console.log(name);
 console.log(".....................");
@@ -47,40 +47,52 @@ console.log(".....................");
 
 // Example of reverse method.
 // It reverse the order no of element.
-let a = [4,5,6,7,8,9];
+let a = [4, 5, 6, 7, 8, 9];
 a.reverse();
 console.log(a);
 console.log("......................");
 
 // Example of sort method.
 // it sorts the element.
-let arr3 =[60,50,80,90,10,12,15,11];
-arr3.sort(function(a,b){
-    return a - b;    //a - b works as asending order
+let arr3 = [60, 50, 80, 90, 10, 12, 15, 11];
+arr3.sort(function (a, b) {
+  return a - b; //a - b works as asending order
 });
 console.log(arr3);
 console.log("......................");
 
 // Example of foreach method
 // it goes to each index separately.
-let array = [34,43,24,42];
-array.forEach(function(val){
-    console.log(val);
+let array = [34, 43, 24, 42];
+array.forEach(function (val) {
+  console.log(val);
 });
 console.log("......................");
 
 // Add 5 in the each index of array.
-let arr4 = [98,67,76,54,35];
-arr4.forEach(function(val){
-    console.log(val + 5);
+let arr4 = [98, 67, 76, 54, 35];
+arr4.forEach(function (val) {
+  console.log(val + 5);
 });
 console.log(".......................");
 
 // Doubled each number using map method.
-let numbers = [2 ,5,8,9,6,];
-let doubled = numbers.map(function(num){
-    return num * 2;
+let numbers = [2, 5, 8, 9, 6];
+let doubled = numbers.map(function (num) {
+  return num * 2;
 });
 console.log(doubled);
 console.log(numbers);
 console.log(".........................");
+
+// Example of fillter() method.
+// Print the number which is greater than 12.
+let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 12, 3, 55, 66, , 44, 78];
+let newnum = num1.filter(function (val) {
+  if (val > 12) return true;
+});
+newnum.sort(function (a, b) {
+  return a - b;
+});
+console.log(newnum);
+console.log("..........................");
--- JS_by_Sheryians Coding School/tempCodeRunnerFile.js
+++ JS_by_Sheryians Coding School/tempCodeRunnerFile.js
@@ -1,8 +1,12 @@
-// Example of sort method.
-// it sorts the element.
-let arr3 =[60,50,80,90,10,12,15,11];
-arr3.sort(function(a,b){
-    return a - b;
+// Example of fillter() method.
+// Print the number which is greater than 12.
+let num1 =[1,2,3,4,5,6,7,8,9,4,5,6,12,3,55,66,,44,78];
+let newnum=num1.filter(function(val){
+    if(val > 12 ) return true;
+    
 });
-console.log(arr3);
-console.log("......................");
+newnum.sort(function(a,b){
+        return a - b;
+    });
+console.log(newnum);
+console.log("..........................");
