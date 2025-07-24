871456af28bbb3af334795f1d76c6fab24d15df1
Rudra swain <rudrasankar96@gmail.com>
2025-07-24 20:23:48 +0530

    Learning object in js

---
+++ JS_by_Sheryians Coding School/Object.js
@@ -0,0 +1,16 @@
+// WAHT IS OBJECT:
+// An object is a collection of related data and functionality — stored as key-value pairs.
+// Think of an object like a real-world "thing" — e.g. a person, car, product, etc., that has properties (data) and actions (methods).
+
+// Example:
+let person ={
+    name: "Rudra",
+    age: 25,
+    isStudent: true,
+    skills: ["JavaScript", "HTML", "CSS"],
+  greet: function() {
+    console.log("Hello, I am " + this.name);
+  }
+};
+console.log(person.name);
+console.log(person.greet());
