521b7007d738d6b4e4c1ae1628ad08c5647e1ef3
Rudra swain <rudrasankar96@gmail.com>
2025-07-30 22:23:59 +0530

    Learning Dom & queryselector

---
+++ JS_by_Sheryians Coding School/DOM/script.js
@@ -0,0 +1,6 @@
+let h1 = document.querySelector("h1");
+console.dir(h1);
+h1.textContent = "Welcome to Sheryians Coding School";  
+let p1 = document.querySelector("p");
+console.dir(p1);
+p1.textContent = "Hi This is my coding shool.";
