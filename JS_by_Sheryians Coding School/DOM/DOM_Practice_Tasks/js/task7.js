c39fe7a2d74057a0e23d19e4eea7b55e45161993
Rudra swain <rudrasankar96@gmail.com>
2025-07-31 22:17:26 +0530

    Add task7: Style multiple list items using querySelectorAll

---
+++ JS_by_Sheryians Coding School/DOM/DOM_Practice_Tasks/js/task7.js
@@ -0,0 +1,8 @@
+const listItems = document.querySelectorAll(".list-item");
+
+listItems.forEach((item) => {
+  item.style.color = "white";
+  item.style.backgroundColor = "teal";
+  item.style.padding = "5px";
+  item.style.marginBottom = "5px";
+});
