efab31688bc99c92e8cf3286d59be186cb1fa6cd
Rudra swain <rudrasankar96@gmail.com>
2025-08-20 22:00:14 +0530

    Example of change in input area

--- JS_by_Sheryians Coding School/DOM/DOM_Practice_Tasks/script.js
+++ JS_by_Sheryians Coding School/DOM/DOM_Practice_Tasks/script.js
@@ -6,7 +6,9 @@ input.addEventListener("input",function(dets){
     }
 })  
 
+// Change event tab chalta heai jab aapka koi input select ya textarea mein koi change ho jaye.
 let see = document.querySelector("select");
+let device = document.querySelector("#device");
 see.addEventListener("change",function(dets){
-    console.log(dets.target.value);
+    device.textContent = `${dets.target.value} Device selected`;
 });
